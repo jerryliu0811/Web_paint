f032320201715160e3b126e40e82ef24af9cca98
jerryliu0811 <jerryliu0811@gmail.com>
2017-08-15 10:29:08 +0800

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,10 +1,10 @@
 <?php
 	session_start();
 
-	$db_host= "dbhome.cs.nctu.edu.tw";
-	$db_name= "chiyi0811_cs_cloud";
-	$db_user= "chiyi0811_cs";
-	$db_password= "5566";
+	$db_host= "";
+	$db_name= "";
+	$db_user= "";
+	$db_password= "";
 	$dsn= "mysql:host=$db_host;dbname=$db_name";
 	$db= new PDO($dsn, $db_user, $db_password);
 ?>
